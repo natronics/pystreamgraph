c14d47c7d68f0fd1969e6913cf4571b36ef083ff
Nathan Bergey <nathan.bergey@gmail.com>
2014-11-29 00:40:23 -0800

    tests run with tox, still broken

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -11,6 +11,8 @@ Tests for `graph` module.
 from __future__ import print_function
 import unittest
 import colorsys
+import random
+import pystreamgraph
 from pystreamgraph import graph
 
 
@@ -23,12 +25,12 @@ class TestGraph(unittest.TestCase):
         self.labels = ["one", "two", "three", "four", "five"]
         for layer in range(5):
             x = [i for i in range(20)]
-            y = [random.uniform(500,10000) for i in range(20)]
-            self.data.append(zip(x,y))
-            self.colors.append(colorsys.hsv_to_rgb(0.588, 0.2, random.uniform(0.4,0.7)))
+            y = [random.uniform(500, 10000) for i in range(20)]
+            self.data.append(zip(x, y))
+            self.colors.append(colorsys.hsv_to_rgb(0.588, 0.2, random.uniform(0.4, 0.7)))
 
     def test_river(self):
-        sg = graph.Stream(self.data, colors=colors, labels=labels)
+        sg = graph.Stream(self.data, colors=self.colors, labels=self.labels)
         sg.draw("Theme_River.test.svg", pystreamgraph.THEME_RIVER, show_labels=True, width=1600, height=600)
 
     def tearDown(self):
