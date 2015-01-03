61a9073c93b051e0aaa262666218ef7167b7eb0b
Nathan Bergey <nathan.bergey@gmail.com>
2015-01-03 10:32:17 -0800

    tox runs green, though main code is commented out. fix zip behavior across python 2,3

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -25,14 +25,13 @@ class TestGraph(unittest.TestCase):
         self.labels = ["one", "two", "three", "four", "five"]
         for layer in range(5):
             x = range(20)
-            y = [random.uniform(500, 10000) for i in range(20)]
-            self.data.append(zip(x, y))
+            y = [random.uniform(500, 10000) for _ in range(20)]
+            self.data.append(list(zip(x, y)))
             self.colors.append(colorsys.hsv_to_rgb(0.588, 0.2, random.uniform(0.4, 0.7)))
 
     def test_river(self):
         sg = graph.Stream(self.data, colors=self.colors, labels=self.labels)
         sg.draw("Theme_River.test.svg", pystreamgraph.THEME_RIVER, show_labels=True, width=1600, height=600)
-
 
     def test_unequaldata(self):
         test_d = []
@@ -41,10 +40,10 @@ class TestGraph(unittest.TestCase):
             if layer == 2:
                 n = 41
             x = range(n)
-            y = [random.uniform(500, 10000) for i in range(n)]
-            test_d.append(zip(x, y))
+            y = [random.uniform(500, 10000) for _ in range(n)]
+            test_d.append(list(zip(x, y)))
+        
         self.assertRaises(Exception, graph.Stream, test_d)
-
 
     def tearDown(self):
         pass
